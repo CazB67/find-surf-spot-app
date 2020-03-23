bcca0d5a8630fc219d38d4a172142765662f4b7b
Caroline <caroline.portilla@gmail.com>
2020-03-23 23:06:38 +0800

    foundation scripts for modal to work

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -3,22 +3,15 @@ $(document).ready(function() {
 
     $(document).foundation();
 
-    //Search button on click event
-    $("#search-btn").on("click", function(){
-       console.log( $("#post-code").val());
-       console.log( $("#ability").val());
-       console.log( $("#when").val());
-       console.log( $("#distance").val());
-
-       //If users don't type in post code it shows up as an empty string 
-       if( $("#post-code").val() === ""){
-        alert("Hi");
-       }
-      
+    $("#search-btn").on("click", function() {
+        var x = new Foundation.Reveal($("#exampleModal1"));
+        x.open();
+        //https://stackoverflow.com/questions/33855505/zurb-foundation-6-reveal-doesnt-work
+        //$('#exampleModal1').foundation('reveal', 'open');
     });
 
     //Results on click event
-    $(".results").on("click", function(event){
+    $(".results").on("click", function(event) {
         alert("Hi");
     });
 
